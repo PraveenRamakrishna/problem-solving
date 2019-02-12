153f1350e949eeb2bf019d9eab6fe8d8683b6493
PraveenKumar <praveen.p.kumar@capgemini.com>
2019-02-12 14:30:24 +0530

    Solution for Largest Palindrome for a 3 digit number

---
+++ src/problem/solving/projecteuler/LargestPalindromeProduct.java
@@ -0,0 +1,36 @@
+package problem.solving.projecteuler;
+
+public class LargestPalindromeProduct {
+	public static void main(String[] args) {
+		System.out.println(largestPalindrome(999));
+	}
+
+	private static long largestPalindrome(int number) {
+		int i = number;		
+		int product = 0;
+		long largetPalindrome=0;
+		while(i>99){
+			int  j = number;
+			while(j>99){
+				product = i*j;
+				if(product==reverse(product)){
+					largetPalindrome = largetPalindrome < product ? product : largetPalindrome;
+					break;
+				}
+				j--;
+			}
+			i--;
+		}
+		return largetPalindrome;
+	}
+
+	static int reverse(int number) {
+		int reverse = 0;
+		while (number > 0) {
+			reverse = reverse * 10;
+			reverse = reverse + number % 10;
+			number = number / 10;
+		}
+		return reverse;
+	}
+}
